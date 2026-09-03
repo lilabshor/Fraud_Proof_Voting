ba9d186a5feb9b4af89911f576adce8aa421f634
lilabshor <anisakarsi@gmail.com>
2026-09-03 09:14:39 +0700

    update process vote

--- proses_vote.php
+++ proses_vote.php
@@ -38,5 +38,33 @@ header("location: index.php");
 exit();
 }
 
-mysqli_stmt_bind_result($stmt_kandidat, $id);
+mysqli_stmt_close($stmt_kandidat);
+
+$stmt_check = mysqli_prepare($conn, "SELECT id FROM kandidat WHERE session_id = ? OR ip_address = ? LIMIT 1");
+mysqli_stmt_bind_param($stmt_check, "ss", $session_id, $ip);
+mysqli_stmt_execute($stmt_check);
+mysqli_stmt_store_result($stmt_check);
+
+if(mysqli_stmt_num_rows($stmt_check) > 0) {
+    mysqli_stmt_close($stmt_check);
+    $_SESSION[`error`] = "Anda sudah pernah melakukan vote!";
+    header("location index.php");
+    exit();
+}
+
+$stmt_insert = mysqli_prepare($conn, "INSERT INTO vote (kandidat_id, ip_address) VALUES (?, ?, ?)");
+mysqli_stmt_bind_param($stmt_insert, "is", $kandidat_id, $ip, $session_id);
+
+if (mysqli_stmt_execute($stmt_insert)) {
+    mysqli_stmt_close($stmt_insert);
+    $_SESSION[`success`] = "Suara anda telah berhasil memilih kandidat terimakasih!";
+    header("location index.php");
+    exit();
+} else {
+    mysqli_stmt_close($stmt_insert);
+    $_SESSION ['error'] = "terjadi kesalahan saat menyimpan vote!";
+    header("location index.php");
+    exit();
+}
+
 
